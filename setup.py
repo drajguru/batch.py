4c13cc50ff7bc14904bb14d06a9ab6951ecdd789
Unknown <moomoohk@ymail.com>
2018-01-13 23:15:42 +0200

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,48 @@
-from setuptools import setup, find_packages
+import sys
+
+import os
+from setuptools import setup, Command
+
+
+# https://github.com/kennethreitz/setup.py/blob/master/setup.py
+from shutil import rmtree
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds...')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution...')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine...')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
 
 setup(
     name='batch',
@@ -9,5 +53,8 @@ setup(
     author='Meshulam Silk',
     author_email='moomoohk@ymail.com',
     description='Batch script (Windows CMD) lexer',
-    install_requires=['ply', 'recordtype']
+    install_requires=['ply', 'recordtype'],
+    cmdclass={
+        'upload': UploadCommand
+    }
 )
